52af6d8c4442ba422c4ec6fafa6844811e97d123
Adrian <adrian.adam@students.bfh.ch>
2022-04-25 16:49:11 +0200

    Fade in Added

--- nonnafink/src/App.js
+++ nonnafink/src/App.js
@@ -7,22 +7,51 @@ import Aktuelles from './components/common//aktuelles/Aktuelles';
 import Galerie from './components/common//galerie/Galerie'
 import {  Routes, Route } from 'react-router-dom';
 import { useLocation } from 'react-router-dom';
+import { useState ,useEffect } from 'react';
 
 
 function App() {
 
 
   const location = useLocation();
-  const is_homeScreen = location.pathname.localeCompare("/") === 0
-
+  const [displayLocation, setDisplayLocation] = useState(location);
+  const [transitionStage, setTransistionStage] = useState("");
+  const is_homeScreen = displayLocation.pathname.localeCompare("/") === 0
   const isBigScreen = window.innerWidth > 1000;
   const isSmallScreen = window.innerWidth < 500;
-  return (
 
+
+
+  console.log("load Component " + transitionStage);
+  useEffect(() => {
+    if (is_homeScreen) {
+      setTransistionStage("fadeIn_landing")
+    }
+    if (location !== displayLocation) {
+      setDisplayLocation(location);
+      if (is_homeScreen) {
+        setTransistionStage("fadeIn")
+      }
+    }
+      
+    console.log("useEffect "+ transitionStage);
+  }, [location, is_homeScreen, displayLocation, transitionStage]);
+
+  return (
+    <div
+      className={`${transitionStage}`}
+      onAnimationEnd={() => {
+        console.log("onAnimationEnd " + transitionStage)
+        if (transitionStage === "fadeOut") {
+          setTransistionStage("fadeIn");
+          setDisplayLocation(location);
+        }
+      }}
+    >
     <div className="App">
       {is_homeScreen ? (<Home></Home>) : (
         <Skeleton isBigScreen={isBigScreen} isSmallScreen={isSmallScreen}>
-          <Routes>
+          <Routes location={displayLocation}>
             <Route path="/" element={isBigScreen ? (<Home></Home>) : (<SmallHome></SmallHome>)}>
             </Route>
             <Route path="/biografie" element={<Biografie></Biografie>}></Route>
@@ -32,6 +61,7 @@ function App() {
           </Routes>
         </Skeleton>
       )}
+      </div>
     </div>
     );
 }
--- nonnafink/src/components/bigScreen/home/Home.js
+++ nonnafink/src/components/bigScreen/home/Home.js
@@ -1,26 +1,26 @@
 
-import React, { Component } from 'react';
+import  { useEffect } from 'react';
 import './Home.css';
 import background from '../../../img/NONNAREGINAFINK-14-F.jpg';
-import { MatomoContext, useMatomo } from '@datapunt/matomo-tracker-react';
+import {  useMatomo } from '@datapunt/matomo-tracker-react';
 import LandingNavigation from '../../common/landing_navigation/LandingNavigation';
 
 
 const Home = () => {
 
-  const { trackPageView, trackEvent } = useMatomo();
+  const { trackPageView } = useMatomo();
 
   // Track page view
-  React.useEffect(() => {
+  useEffect(() => {
     trackPageView();
-  }, []);
+  }, [trackPageView]);
 
   return (
     <div className='home'>
       <div className='navigation-container'>
         <LandingNavigation></LandingNavigation>
       </div>
-      <h1 className='title'>Nonna Fink  <br></br>Schauspielerin</h1>
+      <h1 className='title'>Nonna Fink <br></br>Schauspielerin</h1>
       <img src={background} alt='background'></img>
 
     </div>
